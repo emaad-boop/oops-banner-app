a0a93c4a1f5eb22a5eb9d48b13fa7a826afcd832
emaad-boop <emaadnaiyar208@gmail.com>
2026-02-17 14:13:49 +0530

    Add files via upload

---
+++ print_with_stars.java
@@ -0,0 +1,11 @@
+class print_with_stars{
+    public static void main(String args[]){
+        System.out.println(" *****  "+ " ***** "+  " ***** "+  " ***** ");
+        System.out.println("*     * "+ "*     *"+  " *    *"+  " *     ");
+        System.out.println("*     * "+ "*     *"+  " *    *"+  " *     ");
+        System.out.println("*     * "+ "*     *"+  " ***** "+  " ***** ");
+        System.out.println("*     * "+ "*     *"+  " *     "+  "     * ");
+        System.out.println("*     * "+ "*     *"+  " *     "+  "     * ");
+        System.out.println(" *****  "+ " ***** "+  " *     "+  " ***** ");
+    }
+}
